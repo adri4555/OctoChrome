f83d708d16db37062238d5e387e321928a7bd6fe
adri_205 <lorossaltarines@gmail.com>
2020-12-25 14:38:53 +0100

    Lenguagues añadidos correctamente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClient, HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SigninComponent } from './pages/signin/signin.component';
+
+//Translate module
+import {TranslateLoader, TranslateModule} from "@ngx-translate/core"
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function createTranslateLoader(http: HttpClient){
+  return new TranslateHttpLoader(http, './assets/lang/', '.json')
+}
 
 @NgModule({
   declarations: [
@@ -12,7 +21,15 @@ import { SigninComponent } from './pages/signin/signin.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader:{
+        provide: TranslateLoader,
+        useFactory: (createTranslateLoader),
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
